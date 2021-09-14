96df4672e7e9819452b462aef7940e48fa168a85
kannadiga <alberteinstine975@gmail.com>
2021-09-14 11:13:56 +0530

    14/9/2021

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Solutions of problems solved daily
 
 1)Alien Dictionary(13/09/2021):
 Given a sorted dictionary of an alien language having N words and k starting alphabets of standard dictionary. Find the order of characters in the alien language.
+
+2)Remove Loop from Linked list(14/09/2021):
+Given a linked list of N nodes such that it may contain a loop. Remove the loop from the linked list, if it is present.
---
+++ Solutions/RemoveLoopInLinkedList.java
@@ -0,0 +1,32 @@
+class Solution
+{
+    //Function to remove a loop in the linked list.
+    public static void removeLoop(Node head){
+        if(head==null||head.next==null) return;
+        Node slow=head,fast=head;
+        slow=slow.next;
+        fast=fast.next.next;
+        while(slow!=null && fast!=null && fast.next!=null){
+            if(slow==fast) break;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        if(slow==null || fast==null || fast.next==null) return;
+        if(slow==fast){
+            slow=head;
+            if(slow!=fast){
+                while(slow.next!=fast.next){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                fast.next=null;
+            }
+            else{
+                while(fast.next!=slow)
+                    fast=fast.next;
+                fast.next=null;
+            }
+        }
+        return;
+    }
+}
